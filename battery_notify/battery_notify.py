2545608ed34b7f23726794ca3d9e172abb2b8913
ericlingit <ericlingit@users.noreply.github.com>
2019-03-07 23:04:09 +0900

    Extracted system stat location string

--- battery_notify/battery_notify.py
+++ battery_notify/battery_notify.py
@@ -1,21 +1,25 @@
-import sys
-import subprocess
+from sys import exit
+from subprocess import run
+
+CHARGING_STATUS = '/sys/class/power_supply/ADP1/online'
+BATTERY_CAPACITY = '/sys/class/power_supply/CMB0/capacity'
 
 try:
     # Read charger & battery status
-    with open('/sys/class/power_supply/ADP1/online', 'r') as a:
-        ac_stat = bool(int(a.readline()))
-        print(f'Plugged in: {ac_stat}')
-    with open('/sys/class/power_supply/CMB0/capacity', 'r') as b:
+    with open(CHARGING_STATUS, 'r') as a:
+        charging = bool(int(a.readline()))
+        print(f'Plugged in: {charging}')
+    with open(BATTERY_CAPACITY, 'r') as b:
         bat_pct = int(b.readline())
         print(f'Capacity: {bat_pct}')
-except IOError:
+except Exception as e:
     # Exit on error
-    sys.exit()
+    print(e)
+    exit()
 
 # Determine when to notify start/stop charging.
 # Check whether charging:
-if ac_stat:
+if charging:
     print('Battery is charging.')
     # Check battery percentage
     if bat_pct >= 60:
@@ -27,5 +31,5 @@ else:
         print("Battery below 40%, start charging!")
 
 ### Use notify-send to display a notification pop up.
-subprocess.run(['notify-send', 'hello world'])
+run(['notify-send', 'hello world'])
 
