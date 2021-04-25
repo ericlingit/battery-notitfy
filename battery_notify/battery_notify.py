462a7cc0949894baaf0f026e09377a3810c9cf3e
ericlingit <ericlingit@users.noreply.github.com>
2021-04-25 14:05:11 +0900

    Remove print, add logger

--- battery_notify/battery_notify.py
+++ battery_notify/battery_notify.py
@@ -1,43 +1,48 @@
+import logging
 from sys import exit
 from time import sleep
 from subprocess import run
 
 CHARGING_STATUS = '/sys/class/power_supply/ADP1/online'
 BATTERY_CAPACITY = '/sys/class/power_supply/CMB0/capacity'
+MAX = 80
+MIN = 50
 
-def read_stats():
+log = logging.getLogger()
+
+
+def read_stats() -> dict:
     try:
         # Read charger & battery status
-        with open(CHARGING_STATUS, 'r') as a:
+        with open(CHARGING_STATUS) as a:
             charging = bool(int(a.readline()))
-            print(f'Plugged in: {charging}')
-        with open(BATTERY_CAPACITY, 'r') as b:
+            log.info(f'Plugged in: {charging}')
+        with open(BATTERY_CAPACITY) as b:
             bat_pct = int(b.readline())
-            print(f'Capacity: {bat_pct}')
+            log.info(f'Capacity: {bat_pct}')
         return {'charging': charging, 'bat_pct': bat_pct}
     except Exception as e:
         # Exit on error
-        exit(f'Error reading power stats: {str(e)}')
+        exit(f'Error reading power stats: {e}')
 
 # Run continuously
 while True:
     # Get power stats
     stats = read_stats()
 
-    # Determine when to notify start/stop charging.
-    # Check whether charging:
+    # Determine what to notify.
     if stats['charging']:
-        print('Battery is charging.\n')
+        log.info('Battery is charging.\n')
         # Check battery percentage
-        if stats['bat_pct'] >= 60:
+        if stats['bat_pct'] > MAX:
             # Show notification pop up
-            run(['notify-send', 'Battery above 60%, stop charging!'])
+            run(['notify-send', f'Battery above {MAX}%, stop charging.'])
     else:
-        print('Battery is discharging.\n')
+        log.info('Battery is discharging.\n')
         # Check battery percentage
-        if stats['bat_pct'] <= 40:
+        if stats['bat_pct'] <= MIN:
             # Show notification pop up
-            run(['notify-send', 'Battery below 40%, start charging!'])
+            run(['notify-send', f'Battery below {MIN}%, start charging.'])
 
     # Sleep 5 minutes
     sleep(300)
